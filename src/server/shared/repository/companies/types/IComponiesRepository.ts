7f8dc314702232ce63b952423d1cbf4b045dcac4
MatheusMangueira <matheusmangueira16@hotmail.com>
2024-04-17 18:03:17 -0300

    feat: create interface for repository

---
+++ src/server/shared/repository/companies/types/IComponiesRepository.ts
@@ -0,0 +1,11 @@
+import { CompaniesModel } from '../../../../model/companies/CompaniesModel';
+
+
+export interface ICompaniesRepository {
+  create(company: CompaniesModel): Promise<CompaniesModel>;
+  getAll(): Promise<CompaniesModel[]>;
+  // findAll(): Promise<CompaniesModel[]>;
+  // findById(id: string): Promise<CompaniesModel | null>;
+  // update(companies: CompaniesModel): Promise<CompaniesModel | null>;
+  // delete(id: string): Promise<boolean>;
+}
